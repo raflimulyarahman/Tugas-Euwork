8cf446fa9c271af5354a6d4774b4ae03b96f8dc1
raflimulyarahman <mulyarahman32@gmaill.com>
2025-06-21 15:35:50 +0700

    tamnbah import controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContohController;
+use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
     return view('Ini adalah halaman utama');
